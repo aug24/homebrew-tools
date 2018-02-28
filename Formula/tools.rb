5c6057eaa39b9ddaebd4124541a30ccc38c912b2
Justin Rowles <justin.rowles.casual@guardian.co.uk>
2018-02-28 10:18:19 +0000

    Correct url

--- Formula/tools.rb
+++ Formula/tools.rb
@@ -2,7 +2,7 @@ class Tools < Formula
   desc "Various aws tools"
   homepage "https://github.com/aug24/justin-tools"
   version "0.0.1"
-  url "https://github.com/aug24/justin-tools/releases/download/v0.0.1/justin-tools.tar.gz"
+  url "https://github.com/aug24/justin-tools/releases/download/0.0.1/justin-tools.tar.gz"
   sha256 "adbdf3506b8b4e31719d33cd5eb6b64769ed9522"
 
   def install
