b909f91cebb6bd7841600a991064dd380a28e426
Justin Rowles <justin.rowles.casual@guardian.co.uk>
2018-02-28 10:20:12 +0000

    Correct sja

--- Formula/tools.rb
+++ Formula/tools.rb
@@ -3,7 +3,7 @@ class Tools < Formula
   homepage "https://github.com/aug24/justin-tools"
   version "0.0.1"
   url "https://github.com/aug24/justin-tools/releases/download/0.0.1/justin-tools.tar.gz"
-  sha256 "adbdf3506b8b4e31719d33cd5eb6b64769ed9522"
+  sha256 "c32d5b8fa49e9e41fc5c83ac17a0812a2c1ac604d2178d1129c2d6728091b9f1"
 
   def install
     bin.install "aws-account-number"
