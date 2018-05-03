766290d50c1ef1ce9c049ae3c727bfcdbbe11e87
Justin Rowles <justin.rowles@gmail.com>
2018-05-03 13:23:33 +0100

    Update tools.rb

--- Formula/tools.rb
+++ Formula/tools.rb
@@ -1,13 +1,14 @@
 class Tools < Formula
   desc "Various aws tools"
   homepage "https://github.com/aug24/justin-tools"
-  version "0.0.2"
-  url "https://github.com/aug24/justin-tools/releases/download/0.0.2/justin-tools.tar.gz"
-  sha256 "e97baa3711ec69a9c03ec4543784e50593a1d83443c234b4abf45d05314efc7d"
+  version "0.0.3"
+  url "https://github.com/aug24/justin-tools/releases/download/0.0.3/justin-tools.tar.gz"
+  sha256 "fb7f073e96322eb1780e241247a272f4cea8a86810425b3650bd7271e2f44f0f"
 
   def install
     bin.install "aws-account-number"
     bin.install "aws-account-aliases"
     bin.install "aws-account-finder"
+    bin.install "git-unfuck"
   end
 end
