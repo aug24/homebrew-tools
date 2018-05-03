506a101ee0664dc61879657f0cc16b6d68b043dd
Justin Rowles <justin.rowles@gmail.com>
2018-05-03 13:26:39 +0100

    Update tools.rb

--- Formula/tools.rb
+++ Formula/tools.rb
@@ -3,7 +3,7 @@ class Tools < Formula
   homepage "https://github.com/aug24/justin-tools"
   version "0.0.3"
   url "https://github.com/aug24/justin-tools/releases/download/0.0.3/justin-tools.tar.gz"
-  sha256 "fb7f073e96322eb1780e241247a272f4cea8a86810425b3650bd7271e2f44f0f"
+  sha256 "2c9c2307287fd213981732d72e9b620fff56b66093a5cdc5ffc5ae23f7e2df8a"
 
   def install
     bin.install "aws-account-number"
