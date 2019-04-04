cfb9963c3dde9a620f866f666c6612c606cacc9d
Justin Rowles <justin.rowles@gmail.com>
2019-04-04 14:27:23 +0100

    Correct version.

--- Formula/tools.rb
+++ Formula/tools.rb
@@ -1,7 +1,7 @@
 class Tools < Formula
   desc "Various aws tools"
   homepage "https://github.com/aug24/justin-tools"
-  version "0.0.3"
+  version "0.0.4"
   url "https://github.com/aug24/justin-tools/archive/0.0.4.tar.gz"
   sha256 "568790931efa3f3d98558273f2bd40dfae6add64e9ea9c6156d36c496e4b3d52"
 
